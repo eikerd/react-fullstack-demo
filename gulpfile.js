69ccd10cce0e5eb2302740db86feee83d0f924cf
eikerd <eikerd@esi-estech.com>
2016-05-27 16:30:49 -0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browerify = require('browerify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+
 
 gulp.task('live-server', function(){
 	var server = new LiveServer('server/main.js');
 	server.start();
+})
+
+gulp.task('bundle',function() {
+	return browerify({
+		entries:'app/main.jsx',
+		debug:true
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
 })
 
 gulp.task('serve', ['live-server'], function(){
