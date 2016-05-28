0c20abbdeed7283b6dbeba036567b91d73e61e13
System Administrator <root@v8s-macbook-pro.attlocal.net>
2016-05-28 01:30:27 -0500

    yo

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -1,1 +1,11 @@
 var React = require('react/addons');
+
+module.exports = React.createClass({
+	render:function() {
+		return {
+			<div>
+				<h1> Grocery Litify</h1>
+			</div>
+		}
+	}
+})
--- app/main.jsx
+++ app/main.jsx
@@ -1,3 +1,7 @@
-
+var React = require('react/addons');
 
 console.log("hellow JSX idio");
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />,app)
